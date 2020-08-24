fda23f21b1bc0eb807b1f8b15cfac0d32dec91ce
unknown <rontab68@yahoo.com>
2020-08-24 08:25:32 -0700

    added comments

--- capFirst_Letter.js
+++ capFirst_Letter.js
@@ -1,8 +1,12 @@
 var name = prompt("What's your name?");
+//Upper case the first letter
 var nameUpper = name.slice(0,1);
-var nameLength = name.length;
-var nameLetters = name.slice(1,nameLength);
-// alert(nameLetters);
 
-// alert("Hi " + nameUpper.toUpperCase() + nameLetters);
-alert("Hi " + nameUpper.toUpperCase() + name.slice(1,nameLength));
+//get the nemelength
+var nameLength = name.length;
+
+//get the remaining letters
+var nameLetters = name.slice(1,nameLength);
+
+//Complete code to uppercase the 1sr letter and lower case the rest
+alert("Hi " + nameUpper.toUpperCase() +name.slice(1,nameLength).toLowerCase() );
