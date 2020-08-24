cfadf15657ec80d314cb2e8a69d256ef09f88053
unknown <rontab68@yahoo.com>
2020-08-24 08:11:02 -0700

    new line of code

--- capFirst_Letter.js
+++ capFirst_Letter.js
@@ -4,4 +4,5 @@ var nameLength = name.length;
 var nameLetters = name.slice(1,nameLength);
 // alert(nameLetters);
 
-alert("Hi " + nameUpper.toUpperCase() + nameLetters);
+// alert("Hi " + nameUpper.toUpperCase() + nameLetters);
+alert("Hi " + nameUpper.toUpperCase() + name.slice(1,nameLength));
