dba781bab9ac162cf21c3c6a46c1dca6fb0c35ba
unknown <rontab68@yahoo.com>
2020-08-27 05:43:25 -0700

    line of code

--- robot2.js
+++ robot2.js
@@ -3,14 +3,21 @@ function buyMilk(money){
     console.log("Move up");
     console.log("Move down");
     console.log("You bought " + numberOfBottles(money,1.5) + " bottles of milk");
-    var change = money % 1.5;
+   
 
-    console.log("You have a change " + "of " + "$ " + change);
+    console.log("You have a change " + "of " +  "$ " + remainingChange(money,1.5) );
 }
    
   function numberOfBottles(startingMoney,costPerBottle){
-
   return Math.floor(startingMoney/costPerBottle);
 
 }
+
+function remainingChange(startingMoney,costPerBottle){
+
+   var change =   startingMoney % 1.5;
+   return change;
+
+}
+
 buyMilk(5);
