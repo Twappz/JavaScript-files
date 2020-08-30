fd4680fc8b1705aef14b7ac3670bd97afde84fdb
unknown <rontab68@yahoo.com>
2020-08-30 07:23:43 -0700

    added code samples in bmi

--- bmi.js
+++ bmi.js
@@ -1,3 +1,5 @@
+//First example//
+
 function bmiCalculator(weightKg, heightMtr){
     
      var bmi = Math.round((weightKg/Math.pow(heightMtr,2)));
@@ -6,3 +8,30 @@ function bmiCalculator(weightKg, heightMtr){
 }
 
 bmiCalculator(65,1.8);
+
+//2nd example//
+
+function bmiCalculator (weight, height) {
+    
+    var bmi = Math.round((weight/Math.pow(height,2)));
+   
+    return bmi;
+}
+
+var bmi =bmiCalculator(60,2)
+
+if(bmi < 18.5){
+    alert("Your BMI is " +  bmi +", so you are underweight")
+}
+
+if( bmi>18.5  && bmi <= 24.9 ){
+    alert("Your BMI is " + bmi +", so you have a normal weight")
+}
+
+if(bmi > 24.9){
+    alert("Your BMI is "  + bmi +", so you are overweight")
+}
+
+
+
+
