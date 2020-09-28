686b52632a91f0f23f7338fe36d526fa31c9e8ff
unknown <rontab68@yahoo.com>
2020-09-27 22:58:06 -0700

    new fizz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -4,11 +4,25 @@ var count =1;
 
 function fizzBuzz(){
 
-    output.push(count);
+    if(count % 3 ===0 && count % 5 === 0){
+        output.push("FizzBuzz");
 
-    count ++;
-    
 
+    }
+    else if(count % 3  === 0){
+
+        output.push("Fizz");
+    }
+    else if(count % 5 === 0){
+
+        output.push("Buzz");
+    }
+
+    else{
+
+        output.push(count);
+    }
+
+    count ++
     console.log(output);
-
 }
